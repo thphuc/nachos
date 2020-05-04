9a55aab8be660103ec382eb0da537f6912db7a07
Tran Huy Phuc <phuc.tran@nextfunc.com>
2020-05-04 10:58:19 +0700

    Create userpages1.c

---
+++ code/test/userpages1.c
@@ -0,0 +1,26 @@
+#include "syscall.h" 
+#define THIS "aaa" 
+#define THAT "bbb"
+
+const int N = 10; // Choose it large enough!
+
+void puts(const char *s) {
+  const char *p; 
+  for (p = s; *p != ’\0’; p++) 
+  PutChar(*p); 
+}
+
+void f(void *arg) {
+  const char *s = arg; 
+  int i;
+  for (i = 0; i < N; i++) 
+  puts(s);
+
+  ThreadExit(); 
+}
+
+int main () {
+  ThreadCreate(f, THIS); 
+  f(THAT);
+  ThreadExit();
+}
